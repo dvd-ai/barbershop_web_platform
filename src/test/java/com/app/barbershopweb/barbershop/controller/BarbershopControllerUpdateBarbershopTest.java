a81f83ae2fbc1b6a0b1d5fe69b6d23cb6760db60
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-02-21 18:26:31 +0200

    minor changes

--- src/test/java/com/app/barbershopweb/barbershop/controller/BarbershopControllerUpdateBarbershopTest.java
+++ src/test/java/com/app/barbershopweb/barbershop/controller/BarbershopControllerUpdateBarbershopTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
