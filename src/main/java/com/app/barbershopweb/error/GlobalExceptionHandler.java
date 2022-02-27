4ab22f7126d9a0667e249c799d39d8336be150e0
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-02-27 15:54:37 +0200

    GlobalExceptionHandler handles MethodArgumentNotValidException

--- src/main/java/com/app/barbershopweb/error/GlobalExceptionHandler.java
+++ src/main/java/com/app/barbershopweb/error/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.app.barbershopweb.error;
 
 import com.app.barbershopweb.exception.NotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
@@ -25,6 +28,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .map(
                         cv ->"'" + cv.getPropertyPath().toString().split("\\.")[1]
                                 + "' " + cv.getMessage()
+                )
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(errors));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+        List<String> errors = ex.getFieldErrors()
+                .stream()
+                .map(
+                        fe -> "'" + fe.getObjectName() + "." +
+                                fe.getField() + "' " + fe.getDefaultMessage()
                 )
                 .toList();
 
