51ad1dd8a51cf9de5392c82838fa7733c1889a96
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-03-02 13:23:04 +0200

    renamed

--- src/main/resources/db/migration/V20220206135521__create_table_users_barbershop.sql
+++ src/main/resources/db/migration/V20220206135521__create_table_users_barbershop.sql
@@ -1,10 +1,10 @@
-CREATE TABLE users_barbershop (
-    users_barbershop serial,
+CREATE TABLE workspace (
+    workspace serial,
     user_id int NOT NULL ,
     barbershop_id int NOT NULL ,
     active bool NOT NULL ,
 
-    PRIMARY KEY (users_barbershop),
+    PRIMARY KEY (workspace),
     UNIQUE (user_id, barbershop_id),
 
     FOREIGN KEY (user_id) REFERENCES users(user_id),
