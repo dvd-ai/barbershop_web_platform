93db738f1809da8505aa934381f5ef76f7b83590
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-03-22 13:43:36 +0200

    not empty

--- src/main/java/com/app/barbershopweb/order/reservation/dto/OrderFiltersDto.java
+++ src/main/java/com/app/barbershopweb/order/reservation/dto/OrderFiltersDto.java
@@ -1,10 +1,10 @@
 package com.app.barbershopweb.order.reservation.dto;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record OrderFiltersDto(
-        @NotNull
+        @NotEmpty
         List<Long>barberIds
 ) {
 }
