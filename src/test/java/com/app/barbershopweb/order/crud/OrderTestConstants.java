36ab132f330d5225309ab80e2c3e76d07a53a1eb
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-03-31 16:50:25 +0300

    OrderControllerAddOrderTest finished. OrderTestConstants works for it.

--- src/test/java/com/app/barbershopweb/order/crud/OrderTestConstants.java
+++ src/test/java/com/app/barbershopweb/order/crud/OrderTestConstants.java
@@ -1,31 +1,32 @@
 package com.app.barbershopweb.order.crud;
 
+import com.app.barbershopweb.barbershop.BarbershopTestConstants;
+
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
 public final class OrderTestConstants {
+    public final BarbershopTestConstants btc = new BarbershopTestConstants();
+
     public final int ORDER_FIELD_AMOUNT = 6;
     public final static String ORDERS_URL = "/orders";
 
     public final long INVALID_ORDER_ID = -100L;
     public final long VALID_ORDER_ID = 1L;
-    public final long VALID_BARBERSHOP_ID = 1L;
+    public final long NOT_EXISTING_ORDER_ID = 100_000L;
     public final long VALID_CUSTOMER_ID = 1L;
     public final long VALID_BARBER_ID = 2L;
     public final LocalDateTime VALID_ORDER_DATE = LocalDateTime.of(
-            2022, 3, 30, 12, 0
+            2022, 3, 30,
+            btc.VALID_BARBERSHOP_ENTITY.getWorkTimeFrom()
+                    .getHour(),
+            0
     );
-
-    public final long NOT_EXISTED_ORDER_ID = 100_000L;
-
+    public final LocalDateTime INVALID_ORDER_DATE = VALID_ORDER_DATE.minusHours(5L);
     public final boolean ACTIVE = true;
 
-    public final LocalTime TIME_FROM = LocalTime.of(8, 0, 0);
-    public final LocalTime TIME_TO = LocalTime.of(20, 0, 0);
-
     public final Order VALID_ORDER_ENTITY = new Order(
-            VALID_ORDER_ID, VALID_BARBERSHOP_ID,
+            VALID_ORDER_ID, btc.VALID_BARBERSHOP_ID,
             VALID_BARBER_ID, VALID_CUSTOMER_ID, VALID_ORDER_DATE, ACTIVE
     );
 
@@ -35,22 +36,27 @@ public final class OrderTestConstants {
 //    );
 
 //    public final Order ORDER_ENTITY_NOT_EXISTED_ID = new Order(
-//            NOT_EXISTED_ORDER_ID, VALID_USER_ID,
+//            NOT_EXISTING_ORDER_ID, VALID_USER_ID,
 //            VALID_BARBERSHOP_ID, ACTIVE
 //    );
 
-//    public final OrderDto VALID_ORDER_DTO = new OrderDto(
-//            VALID_ORDER_ID, VALID_USER_ID,
-//            VALID_BARBERSHOP_ID, ACTIVE
-//    );
+    public final OrderDto VALID_ORDER_DTO = new OrderDto(
+            VALID_ORDER_ID, btc.VALID_BARBERSHOP_ID,
+            VALID_BARBER_ID, VALID_CUSTOMER_ID, VALID_ORDER_DATE, ACTIVE
+    );
 
-//    public final OrderDto INVALID_ORDER_DTO = new OrderDto(
-//            INVALID_ORDER_ID, -100L,
-//            0L, ACTIVE
-//    );
+    public final OrderDto INVALID_ORDER_DTO = new OrderDto(
+            0L, 0L,
+            0L, 0L, VALID_ORDER_DATE, ACTIVE
+    );
+
+    public final OrderDto INVALID_BDF_ORDER_DTO = new OrderDto(
+            VALID_ORDER_ID, btc.VALID_BARBERSHOP_ID,
+            VALID_CUSTOMER_ID, VALID_CUSTOMER_ID, INVALID_ORDER_DATE, ACTIVE
+    );
 
 //    public final OrderDto ORDER_DTO_NOT_EXISTED_ID = new OrderDto(
-//            NOT_EXISTED_ORDER_ID, VALID_USER_ID,
+//            NOT_EXISTING_ORDER_ID, VALID_USER_ID,
 //            VALID_BARBERSHOP_ID, ACTIVE
 //    );
 
@@ -65,17 +71,17 @@ public final class OrderTestConstants {
     //note: barbershop should work at VALID_ORDER_DATE & the barber can work there
     public final List<Order> VALID_ORDER_ENTITY_LIST = List.of(
             new Order(
-                    VALID_ORDER_ID, VALID_BARBERSHOP_ID,
+                    VALID_ORDER_ID, btc.VALID_BARBERSHOP_ID,
                     barberIds.get(0), customerIds.get(0),
                     VALID_ORDER_DATE, ACTIVE
             ),
             new Order(
-                    VALID_ORDER_ID + 1, VALID_BARBERSHOP_ID,
+                    VALID_ORDER_ID + 1, btc.VALID_BARBERSHOP_ID,
                     barberIds.get(0), customerIds.get(0),
                     VALID_ORDER_DATE.plusDays(1L), ACTIVE
             ),
             new Order(
-                    VALID_ORDER_ID + 2, VALID_BARBERSHOP_ID + 1,
+                    VALID_ORDER_ID + 2, btc.VALID_BARBERSHOP_ID + 1,
                     barberIds.get(1), customerIds.get(1),
                     VALID_ORDER_DATE, ACTIVE
             )
@@ -85,17 +91,17 @@ public final class OrderTestConstants {
     //note: barbershop should work at VALID_ORDER_DATE & the barber can work there
     public final List<OrderDto> VALID_ORDER_DTO_LIST = List.of(
             new OrderDto(
-                    VALID_ORDER_ID, VALID_BARBERSHOP_ID,
+                    VALID_ORDER_ID, btc.VALID_BARBERSHOP_ID,
                     barberIds.get(0), customerIds.get(0),
                     VALID_ORDER_DATE, ACTIVE
             ),
             new OrderDto(
-                    VALID_ORDER_ID + 1, VALID_BARBERSHOP_ID,
+                    VALID_ORDER_ID + 1, btc.VALID_BARBERSHOP_ID,
                     barberIds.get(0), customerIds.get(0),
                     VALID_ORDER_DATE.plusDays(1L), ACTIVE
             ),
             new OrderDto(
-                    VALID_ORDER_ID + 2, VALID_BARBERSHOP_ID + 1,
+                    VALID_ORDER_ID + 2, btc.VALID_BARBERSHOP_ID + 1,
                     barberIds.get(1), customerIds.get(1),
                     VALID_ORDER_DATE, ACTIVE
             )
@@ -104,21 +110,40 @@ public final class OrderTestConstants {
     //ERROR MESSAGES:
 
     //CV means 'constraint violation'
-    public final String DTO_CV_ORDER_ID_ERR_MSG = "'workspaceDto.workspaceId' must be greater than or equal to 1";
-    public final String DTO_CV_USER_ID_ERR_MSG = "'workspaceDto.userId' must be greater than or equal to 1";
-    public final String DTO_CV_BARBERSHOP_ID_ERR_MSG = "'workspaceDto.barbershopId' must be greater than or equal to 1";
+    public final String DTO_CV_ORDER_ID_ERR_MSG = "'orderDto.orderId' must be greater than or equal to 1";
+    public final String DTO_CV_CUSTOMER_ID_ERR_MSG = "'orderDto.customerId' must be greater than or equal to 1";
+    public final String DTO_CV_BARBER_ID_ERR_MSG = "'orderDto.barberId' must be greater than or equal to 1";
+    public final String DTO_CV_BARBERSHOP_ID_ERR_MSG = "'orderDto.barbershopId' must be greater than or equal to 1";
+
 
     //PV means 'path variable'
-    public final String PV_ORDER_ID_ERR_MSG = "'workspaceId' must be greater than or equal to 1";
+    public final String PV_ORDER_ID_ERR_MSG = "'orderId' must be greater than or equal to 1";
 
-//    //fk, uk violation
-//    public final String FK_CV_USER_ID_ERR_MSG = "fk violation: user with id " +
-//            VALID_ORDER_DTO.userId()  + " not present";
-//
-//    public final String FK_CV_BARBERSHOP_ID_ERR_MSG = "fk violation: barbershop with id " +
-//            VALID_ORDER_DTO.barbershopId()  + " not present";
-//
-//    public final String UK_CV_ERR_MSG = "uk violation: workspace with user id " + VALID_ORDER_DTO.userId() +
-//            " and barbershop id " + VALID_ORDER_DTO.barbershopId() + " already exists.";
+    //fk, uk violation
+    public final String FK_CV_BARBER_ID_ERR_MSG = "fk violation: barber with id " +
+            VALID_ORDER_DTO.barberId()  + " doesn't work at barbershop with id " + VALID_ORDER_DTO.barbershopId();
+
+    public final String FK_CV_CUSTOMER_ID_ERR_MSG = "fk violation: customer with id " +
+            VALID_ORDER_DTO.customerId()  + " not present";
+
+    public final String UK_CV_BARBER_ID_ORDER_DATE_ERR_MSG = "uk violation: order with barberId " + VALID_ORDER_DTO.barberId() +
+            " and orderDate " + VALID_ORDER_DTO.orderDate() + " already exists.";
+
+    public final String UK_CV_CUSTOMER_ID_ORDER_DATE_ERR_MSG =
+            "uk violation: order with customerId "
+            + VALID_ORDER_DTO.customerId() +
+            " and orderDate " + VALID_ORDER_DTO.orderDate() + " already exists.";
+
+    //BDF - means business data format
+    public final String BDF_CV_CUSTOMER_ID_BARBER_ID_EQ_ERR_MSG =
+            "Order with id " + INVALID_BDF_ORDER_DTO.orderId() +
+                    " shouldn't have equal customerId and barberId";
+
+    public final String BDF_CV_BARBERSHOP_HOURS_ERR_MSG =
+            "orderDate with time " + INVALID_BDF_ORDER_DTO.orderDate().
+                    toLocalTime() + " violates barbershop order hours (" +
+                    btc.VALID_BARBERSHOP_ENTITY.getWorkTimeFrom() + " - " +
+                    btc.VALID_BARBERSHOP_ENTITY.getWorkTimeTo().minusHours(1L) +
+                    ")";
 
 }
---
+++ src/test/java/com/app/barbershopweb/order/crud/controller/OrderControllerAddOrderTest.java
@@ -0,0 +1,186 @@
+package com.app.barbershopweb.order.crud.controller;
+
+import com.app.barbershopweb.exception.DbUniqueConstraintsViolationException;
+import com.app.barbershopweb.exception.NotFoundException;
+import com.app.barbershopweb.order.crud.OrderController;
+import com.app.barbershopweb.order.crud.OrderConverter;
+import com.app.barbershopweb.order.crud.OrderService;
+import com.app.barbershopweb.order.crud.OrderTestConstants;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static com.app.barbershopweb.order.crud.OrderTestConstants.ORDERS_URL;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OrderController.class)
+@DisplayName("Testing POST: " + ORDERS_URL)
+class OrderControllerAddOrderTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    OrderService orderService;
+
+    @MockBean
+    OrderConverter orderConverter;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private final OrderTestConstants otc = new OrderTestConstants();
+
+    @DisplayName("when order dto isn't valid " +
+            "returns status code 400 & error dto")
+    @Test
+    void whenOrderDtoNotValid() throws Exception {
+        String json = objectMapper.writeValueAsString(
+                otc.INVALID_ORDER_DTO
+        );
+
+        mockMvc
+                .perform(post(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(4)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.DTO_CV_BARBERSHOP_ID_ERR_MSG)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.DTO_CV_ORDER_ID_ERR_MSG)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.DTO_CV_CUSTOMER_ID_ERR_MSG)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.DTO_CV_BARBER_ID_ERR_MSG)));
+    }
+
+    @DisplayName("when order dto violates db fk constraints " +
+            "returns status code 404 & error dto")
+    @Test
+    void whenOrderDtoViolatesDbFkConstraints() throws Exception {
+        String json = objectMapper.writeValueAsString(
+                otc.VALID_ORDER_DTO
+        );
+
+        when(orderConverter.mapToEntity(any())).thenReturn(
+                otc.VALID_ORDER_ENTITY);
+
+        when(orderService.addOrder(any())).thenThrow(
+                new NotFoundException(
+                        List.of(
+                                otc.FK_CV_BARBER_ID_ERR_MSG,
+                                otc.FK_CV_CUSTOMER_ID_ERR_MSG
+                        )
+                )
+        );
+
+        mockMvc
+                .perform(post(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(2)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.FK_CV_BARBER_ID_ERR_MSG)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.FK_CV_CUSTOMER_ID_ERR_MSG)));
+    }
+
+    @DisplayName("when order dto violates db uk constraints " +
+            "returns status code 400 & error dto")
+    @Test
+    void whenOrderDtoViolatesDbUkConstraints() throws Exception {
+        String json = objectMapper.writeValueAsString(
+                otc.VALID_ORDER_DTO
+        );
+
+        when(orderConverter.mapToEntity(any())).thenReturn(
+                otc.VALID_ORDER_ENTITY);
+
+        when(orderService.addOrder(any())).thenThrow(
+                new DbUniqueConstraintsViolationException(
+                        List.of(
+                                otc.UK_CV_BARBER_ID_ORDER_DATE_ERR_MSG,
+                                otc.UK_CV_CUSTOMER_ID_ORDER_DATE_ERR_MSG
+                        )
+                )
+        );
+
+        mockMvc
+                .perform(post(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(2)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.UK_CV_BARBER_ID_ORDER_DATE_ERR_MSG)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.UK_CV_CUSTOMER_ID_ORDER_DATE_ERR_MSG)));
+    }
+
+    @DisplayName("when order dto violates business data format " +
+            "returns status code 400 & error dto")
+    @Test
+    void whenOrderDtoViolatesBusinessDataFormat() throws Exception {
+        String json = objectMapper.writeValueAsString(
+                otc.VALID_ORDER_DTO
+        );
+
+        when(orderConverter.mapToEntity(any())).thenReturn(
+                otc.VALID_ORDER_ENTITY);
+
+        when(orderService.addOrder(any())).thenThrow(
+                new DbUniqueConstraintsViolationException(
+                        List.of(
+                                otc.UK_CV_BARBER_ID_ORDER_DATE_ERR_MSG,
+                                otc.UK_CV_CUSTOMER_ID_ORDER_DATE_ERR_MSG
+                        )
+                )
+        );
+
+        mockMvc
+                .perform(post(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(2)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.UK_CV_BARBER_ID_ORDER_DATE_ERR_MSG)))
+                .andExpect(jsonPath("$.errors", hasItem(otc.UK_CV_CUSTOMER_ID_ORDER_DATE_ERR_MSG)));
+    }
+
+
+
+    @DisplayName("after saving order entity returns its id and status code 201")
+    @Test
+    void shouldAddOrder() throws Exception {
+        String json = objectMapper.writeValueAsString(
+                otc.VALID_ORDER_DTO
+        );
+
+        when(orderConverter.mapToEntity(any())).thenReturn(
+                otc.VALID_ORDER_ENTITY
+        );
+        when(orderService.addOrder(any())).thenReturn(
+                otc.VALID_ORDER_ID
+        );
+
+        mockMvc
+                .perform(post(ORDERS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().string(String.valueOf(otc.VALID_ORDER_ID)));
+    }
+}
