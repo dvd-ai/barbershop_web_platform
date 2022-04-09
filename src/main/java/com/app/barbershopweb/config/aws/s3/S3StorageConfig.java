016ec809c0f48be6a246a0c9990398a32f49d583
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-04-09 18:21:19 +0300

    S3StorageConfig, added env vars (aws credentials) into docker-compose.yml

---
+++ src/main/java/com/app/barbershopweb/config/aws/s3/S3StorageConfig.java
@@ -0,0 +1,30 @@
+package com.app.barbershopweb.config.aws.s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3StorageConfig {
+    @Value("${aws.service.credentials.access.key.id}")
+    private String accessKeyId;
+
+    @Value("${aws.service.credentials.secret.access.key}")
+    private String secretAccessKey;
+
+    @Bean
+    public AmazonS3 s3client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+    }
+
+}
