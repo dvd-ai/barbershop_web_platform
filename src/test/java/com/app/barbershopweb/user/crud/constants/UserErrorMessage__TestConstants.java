23847d3a7aa9c8970e4a4636259309475801fe9f
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-04-12 18:47:42 +0300

    renamed

--- src/test/java/com/app/barbershopweb/user/crud/constants/UserErrorMessage__TestConstants.java
+++ src/test/java/com/app/barbershopweb/user/crud/constants/UserErrorMessage__TestConstants.java
@@ -9,5 +9,5 @@ public final class UserErrorMessage__TestConstants {
     public static final String USER_ERR_INVALID_DTO_LAST_NAME = "'usersDto.lastName' must not be blank";
 
     public static final String USER_ERR_INVALID_PATH_VAR_USER_ID = "'userId' must be greater than or equal to 1";
-    public static final String USER_ERR_NOT_EXISTING_USER_ID = "User with id " + USERS_NOT_EXISTED_USER_ID + " not found";
+    public static final String USER_ERR_NOT_EXISTING_USER_ID = "User with id " + USERS_NOT_EXISTING_USER_ID + " not found";
 }
