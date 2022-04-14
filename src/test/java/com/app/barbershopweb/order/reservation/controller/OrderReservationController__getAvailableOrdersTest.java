bf71e70a67ce5e9a436eaabf42b490d4eaace8e5
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2022-04-14 15:59:37 +0300

    shortened

--- src/test/java/com/app/barbershopweb/order/reservation/controller/OrderReservationController__getAvailableOrdersTest.java
+++ src/test/java/com/app/barbershopweb/order/reservation/controller/OrderReservationController__getAvailableOrdersTest.java
@@ -4,6 +4,8 @@ import com.app.barbershopweb.order.crud.OrderConverter;
 import com.app.barbershopweb.order.reservation.OrderReservationController;
 import com.app.barbershopweb.order.reservation.OrderReservationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,18 +13,23 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static com.app.barbershopweb.order.crud.constants.OrderMetadata__TestConstants.ORDER_FIELD_AMOUNT;
 import static com.app.barbershopweb.order.reservation.constants.OrderReservation_Metadata__TestConstants.ORDER_RESERVATION_URL;
 import static com.app.barbershopweb.order.reservation.constants.dto.OrderReservation_GetOpenOrders_Dto__TestConstants.GET_OPEN_ORDERS__REQUEST_VALID_DTO;
 import static com.app.barbershopweb.order.reservation.constants.list.dto.OrderReservation_List_OrderDto__TestConstants.ORDER_RESERVATION_OPEN_ORDER_DTO_LIST;
 import static com.app.barbershopweb.order.reservation.constants.list.entity.OrderReservation_List_OrderEntity__TestConstants.ORDER_RESERVATION_OPEN_ORDER_ENTITY_LIST;
-import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -44,10 +51,7 @@ class OrderReservationController__getAvailableOrdersTest {
 
     @Autowired
     ObjectMapper objectMapper;
-
-
-    String json;
-
+    
     @Test
     @DisplayName(
             """
@@ -56,7 +60,7 @@ class OrderReservationController__getAvailableOrdersTest {
                     """
     )
     void shouldReturnEmptyUnreservedOrdersList() throws Exception {
-        json = objectMapper.writeValueAsString(
+        String json = objectMapper.writeValueAsString(
                 GET_OPEN_ORDERS__REQUEST_VALID_DTO
         );
 
@@ -87,7 +91,7 @@ class OrderReservationController__getAvailableOrdersTest {
                     """
     )
     void shouldReturnUnreservedOrdersList() throws Exception {
-        json = objectMapper.writeValueAsString(
+        String json = objectMapper.writeValueAsString(
                 GET_OPEN_ORDERS__REQUEST_VALID_DTO
         );
 
@@ -105,48 +109,27 @@ class OrderReservationController__getAvailableOrdersTest {
         )
                 .thenReturn(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST);
 
-        mockMvc.perform(post(ORDER_RESERVATION_URL)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json)
-                )
-                .andDo(print())
-                .andExpect(status().isOk())
+        MockHttpServletResponse response = mockMvc.perform(post(ORDER_RESERVATION_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+        ).andReturn().getResponse();
+        
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        
+        DocumentContext context = JsonPath.parse(response.getContentAsString());
+        List<Object> object = context.read("$");
+        
+        assertEquals(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.size(), object.size());
 
-                .andExpect(jsonPath("$[0].orderId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(0).orderId().intValue())))
-                .andExpect(jsonPath("$[0].barbershopId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(0).barbershopId().intValue())))
-                .andExpect(jsonPath("$[0].barberId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(0).barberId().intValue())))
-                .andExpect(jsonPath("$[0].customerId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(0).customerId())))
-                .andExpect(jsonPath("$[0].orderDate", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(0).orderDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))))
-                .andExpect(jsonPath("$[0].active", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(0).active())))
-
-                .andExpect(jsonPath("$[1].orderId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(1).orderId().intValue())))
-                .andExpect(jsonPath("$[1].barbershopId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(1).barbershopId().intValue())))
-                .andExpect(jsonPath("$[1].barberId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(1).barberId().intValue())))
-                .andExpect(jsonPath("$[1].customerId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(1).customerId())))
-                .andExpect(jsonPath("$[1].orderDate", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(1).orderDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))))
-                .andExpect(jsonPath("$[1].active", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(1).active())))
-
-                .andExpect(jsonPath("$[2].orderId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(2).orderId().intValue())))
-                .andExpect(jsonPath("$[2].barbershopId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(2).barbershopId().intValue())))
-                .andExpect(jsonPath("$[2].barberId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(2).barberId().intValue())))
-                .andExpect(jsonPath("$[2].customerId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(2).customerId())))
-                .andExpect(jsonPath("$[2].orderDate", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(2).orderDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))))
-                .andExpect(jsonPath("$[2].active", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(2).active())))
-
-                .andExpect(jsonPath("$[3].orderId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(3).orderId().intValue())))
-                .andExpect(jsonPath("$[3].barbershopId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(3).barbershopId().intValue())))
-                .andExpect(jsonPath("$[3].barberId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(3).barberId().intValue())))
-                .andExpect(jsonPath("$[3].customerId", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(3).customerId())))
-                .andExpect(jsonPath("$[3].orderDate", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(3).orderDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))))
-                .andExpect(jsonPath("$[3].active", is(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(3).active())))
-
-                .andExpect(jsonPath("$", hasSize(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.size())))
-                .andExpect(jsonPath("$[0]", aMapWithSize(ORDER_FIELD_AMOUNT)))
-                .andExpect(jsonPath("$[1]", aMapWithSize(ORDER_FIELD_AMOUNT)))
-                .andExpect(jsonPath("$[2]", aMapWithSize(ORDER_FIELD_AMOUNT)))
-                .andExpect(jsonPath("$[3]", aMapWithSize(ORDER_FIELD_AMOUNT)))
-        ;
-
+        for (var i = 0; i < ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.size(); i++) {
+            Map<String, Object> dto = context.read("$[" + i + "]");
+            assertEquals(ORDER_FIELD_AMOUNT, dto.size());
+            assertEquals(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(i).orderId().intValue(), (Integer) context.read("$["+ i + "].orderId"));
+            assertEquals(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(i).barbershopId().intValue(), (Integer) context.read("$["+ i + "].barbershopId"));
+            assertEquals(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(i).barberId().intValue(), (Integer) context.read("$["+ i + "].barberId"));
+            assertEquals(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(i).customerId(), context.read("$["+ i + "].customerId"));
+            assertEquals(ORDER_RESERVATION_OPEN_ORDER_DTO_LIST.get(i).orderDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), context.read("$["+ i + "].orderDate"));
+        }
     }
 
 
